25b77b80cd1fd2328b9f4ccdca84d426bd4392fc
caiomelotec <caio.melotec@icloud.com>
2024-01-29 02:45:48 +0100

    db connection

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+
+mongoose
+  .connect("mongodb://localhost:27017/auth")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.listen(8080, () => {
   console.log("Server running on port 8080");
 });
