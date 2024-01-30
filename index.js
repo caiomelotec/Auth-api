cde82f9463dcfc9096adb061706f03b671e2bee8
caiomelotec <caio.melotec@icloud.com>
2024-01-30 03:24:00 +0100

    cors config

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 
 const UserModel = require("./models/User");
@@ -13,8 +14,14 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Working...");
