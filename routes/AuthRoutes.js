219ffce3a77346c3966cde709309ccb01bb6d3d5
caiomelotec <caio.melotec@icloud.com>
2024-02-01 01:53:44 +0100

    login, logout, google router

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const AuthControllers = require("../controllers/AuthController");
 
 router.post("/register", AuthControllers.register);
+router.post("/login", AuthControllers.login);
+router.post("/google", AuthControllers.google);
+router.post("/logout", AuthControllers.logout);
 
 module.exports = router;
