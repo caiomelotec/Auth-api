0a36fa03ac37a4a7c96c7725279ad85e92effcdd
caiomelotec <caio.melotec@icloud.com>
2024-02-04 21:07:01 +0100

    fetch userById route

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const verifyUser = require("../util/verifyUser");
 const UserControllers = require("../controllers/UserController");
 
-router.get("/", UserControllers.hello);
+
+router.get("/user", UserControllers.fetchUserById);
 router.put("/updateuser", verifyUser, UserControllers.updateUser);
 
 module.exports = router;
