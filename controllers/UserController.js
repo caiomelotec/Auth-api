1f80588e21845bf9936cbc9d08d0024fd652634f
caiomelotec <caio.melotec@icloud.com>
2024-02-20 01:50:42 +0100

    clear user controllers

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -8,8 +8,6 @@ exports.hello = (req, res) => {
 exports.updateUser = async (req, res) => {
   const userId = req.session.userId;
 
-  console.log(userId);
-
   try {
     const updatedUser = await UserModel.findOneAndUpdate(
       { _id: userId },
@@ -27,11 +25,9 @@ exports.updateUser = async (req, res) => {
 
 exports.fetchUserById = async (req, res) => {
   const userId = req.session.userId;
-  console.log("this is the session:", req.session);
   try {
     const user = await UserModel.findOne({ _id: userId });
 
-    console.log(user);
     const { username, _id, email, profileImg } = user;
     res.status(200).send({ username, id: _id, email, profileImg });
   } catch (err) {
